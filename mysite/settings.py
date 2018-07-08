cbc9010671eb34a66cf6acaa5a51f2f7092a2e2a
Lauren He <laurenheren@gmail.com>
2018-07-08 13:51:45 -0700

    Switched debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q4*j@*60l(gp#43_n&#u!!)@qix%^zln+v$v477$55a=oa85n0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
